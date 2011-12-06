4159ad2bac7a3d0e9711f2aedc2cac8bd9eda09e
Shom <webdesign.banerji@gmail.com>
2011-12-05 21:11:56 -0500

    All functionality for Companies finalized
    All CRU features for Company and CRUD for Division has been completed. This commit marks the completion of the Company section. However testing and bug fixes may be addressed later.

--- app/models/division.rb
+++ app/models/division.rb
@@ -22,8 +22,8 @@ class Division < ActiveRecord::Base
   def update_store_counts
     if company_id_changed?
       Store.update_all({:company_id => self[:company_id]},{:company_id => company_id_was, :division_id => self[:id]})
-      Company.update_counters company_id_was, :stores_count => (-1)*self[:stores_count]
-      Company.update_counters self[:company_id], :stores_count => self[:stores_count]      
+      Company.update_counters company_id_was, :stores_count => (-1)*self[:stores_count], :divisions_count => -1
+      Company.update_counters self[:company_id], :stores_count => self[:stores_count], :divisions_count => 1   
     end
   end
 end
