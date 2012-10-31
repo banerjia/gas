3688612878712794d27e0f91f9a6b0c37eb0a5ab
Abhishek Banerji <webdesign.banerji@gmail.com>
2012-10-31 05:29:36 -0400

    Sample Mailer & Other Changes

--- Gemfile
+++ Gemfile
@@ -20,4 +20,4 @@ group :test do
   # Pretty printed test output
   gem 'turn', :require => false
 end
-
+gem 'roadie'
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -2,7 +2,7 @@ Graeters::Application.configure do
   # Settings specified here will take precedence over those in config/application.rb
 
   # In the development environment your application's code is reloaded on
-  # every request.  This slows down response time but is perfect for development
+  # every request. This slows down response time but is perfect for development
   # since you don't have to restart the web server when you make code changes.
   config.cache_classes = false
 
@@ -11,10 +11,20 @@ Graeters::Application.configure do
 
   # Show full error reports and disable caching
   config.consider_all_requests_local       = true
-  config.action_controller.perform_caching = false 
+  config.action_controller.perform_caching = false
 
   # Don't care if the mailer can't send
-  config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.default_url_options = { host: "railscasts.com" }
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    address: "smtp.gmail.com",
+    port: 587,
+    authentication: "plain",
+    enable_starttls_auto: true,
+    user_name: 'webdesign.banerji@gmail.com',
+    password: 'dnmkac12'
+  }
 
   # Print deprecation notices to the Rails logger
   config.active_support.deprecation = :log
@@ -22,25 +32,16 @@ Graeters::Application.configure do
   # Only use best-standards-support built into browsers
   config.action_dispatch.best_standards_support = :builtin
 
+  # Raise exception on mass assignment protection for Active Record models
+  config.active_record.mass_assignment_sanitizer = :strict
+
+  # Log the query plan for queries taking more than this (works
+  # with SQLite, MySQL, and PostgreSQL)
+  config.active_record.auto_explain_threshold_in_seconds = 0.5
+
   # Do not compress assets
-  config.assets.compress = true
-  #config.assets.precompile = [/^[^_]/]
-  config.assets.digest = false 
-  config.assets.js_compressor = :uglifier
-  config.assets.css_compressor = :yui
+  config.assets.compress = false
 
   # Expands the lines which load the assets
   config.assets.debug = true
-  # config.action_controller.asset_host = "http://assets.abhishekbanerji.com"
-  
-  
-  # For Rails 3.2 Upgrade
- # Raise exception on mass assignment protection for Active Record models
-config.active_record.mass_assignment_sanitizer = :strict
-
-# Log the query plan for queries taking more than this (works
-# with SQLite, MySQL, and PostgreSQL)
-config.active_record.auto_explain_threshold_in_seconds = 0.5
-
-config.log_tags = [:uuid, :remote_ip]
 end
