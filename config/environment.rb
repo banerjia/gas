6bd501c373fb5f86267ab0e006906791f50f18ce
Abhishek Banerji <webdesign.banerji@gmail.com>
2012-11-19 20:38:30 -0500

    Update config/environment.rb
    Removed configuration for thinking_sphinx

--- config/environment.rb
+++ config/environment.rb
@@ -3,8 +3,4 @@ ENV["RAILS_ENV"] = 'development'
 # Load the rails application
 require File.expand_path('../application', __FILE__)
 # Initialize the rails application
-Graeters::Application.initialize! do |config|
-	config.gem( 
-		'thinking-sphinx', :version => '2.0.13'
-	)
-end 
+Graeters::Application.initialize! 
