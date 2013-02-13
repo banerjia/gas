6e4a868984546f9eb8a2081c8259cad4b5b34425
Abhishek Banerji <webdesign.banerji@gmail.com>
2013-02-13 01:44:56 -0500

    Audits: Work in Progress 4

--- app/models/audit.rb
+++ app/models/audit.rb
@@ -1,89 +1,89 @@
 class Audit < ActiveRecord::Base
-  include Tire::Model::Search
-  include Tire::Model::Callbacks
-  
-  belongs_to :store, :counter_cache => true
-  
-  has_many :metrics
-  has_many :audit_journal
-  
-  tire do
-    index_name('audits')
-    mapping do
-      indexes :id,	:type=>'integer', :index => 'not_analyzed'
-      indexes :store_id, :type => 'integer', :index => 'not_analyzed'
-      indexes :company_id, :type => 'integer', :index => 'not_analyzed', :as => 'store.company_id'
-      indexes :company_name, :type => 'string', :index => 'not_analyzed', :as => 'store.company[:name]'
-      indexes :store_name, :type => 'string', :analyzer => 'snowball', :as => 'store.name_with_locality'
-      indexes :auditor_facet, :type => 'string', :index => 'not_analyzed', :as => 'auditor_name', :include_in_all => false
-      indexes :auditor_name, :type => 'string', :analyzer => 'snowball'
-      indexes :score, :type => 'integer',  :index => 'not_analyzed'
-      indexes :pending, :type => 'boolean', :index => 'not_analyzed', :as => 'is_pending?'
-      indexes :created_at, :type => 'date', :index => 'not_analyzed'
-    end
-  end
-  
-  def self.search_audits( params )
-    return_value = Hash.new
-    tire_results = tire.search :load => {:include => ['store']}, :per_page => params[:per_page], :page => params[:page] do 
-      query do
-	boolean do
-	  must { string params[:q] || all} 
-	  must { string "company_id:#{params[:company_id]}" } if params[:company_id].present?
-	  must { string "auditor_facet:#{params[:auditor]}" } if params[:auditor].present?
-	  must { string "store_id:#{params[:store]}" } if params[:store].present?
-	  must { range :score, from: params[:score_lower], to: params[:score_upper]} if params[:score_lower].present?
+	include Tire::Model::Search
+	include Tire::Model::Callbacks
+	
+	belongs_to :store, :counter_cache => true
+	
+	has_many :metrics
+	has_many :audit_journal
+	
+	tire do
+		index_name('audits')
+		mapping do
+			indexes :id,	          :type=>'integer',     :index => 'not_analyzed'
+			indexes :store_id,      :type => 'integer',   :index => 'not_analyzed'
+			indexes :company_id,    :type => 'integer',   :index => 'not_analyzed',   :as => 'store.company_id'
+			indexes :company_name,  :type => 'string',    :index => 'not_analyzed',   :as => 'store.company[:name]'
+			indexes :store_name,    :type => 'string',    :analyzer => 'snowball',    :as => 'store.name_with_locality'
+			indexes :auditor_facet, :type => 'string',    :index => 'not_analyzed',   :as => 'auditor_name',            :include_in_all => false
+			indexes :auditor_name,  :type => 'string',    :analyzer => 'snowball'
+			indexes :score,         :type => 'integer',	  :index => 'not_analyzed'
+			indexes :pending,       :type => 'boolean',   :index => 'not_analyzed',   :as => 'is_pending?'
+			indexes :created_at,    :type => 'date',      :index => 'not_analyzed'
+		end
 	end
-      end
+	
+	def self.search_audits( params )
+		return_value = Hash.new
+		tire_results = tire.search :load => {:include => ['store']}, :per_page => params[:per_page], :page => params[:page] do 
+			query do
+      	boolean do
+      		must { string params[:q] || all} 
+      		must { term :company_id, params[:company_id] }  if params[:company_id].present?
+      		must { term :auditor_facet, params[:auditor] }  if params[:auditor].present?
+      		must { term :store_id, params[:store] }         if params[:store].present?
+      		must { range :score, from: params[:score_lower], to: params[:score_upper]} if params[:score_lower].present?
+      	end
+			end
 
-      facet('chains') {terms :company_id }
-      facet('auditors') {terms :auditor_facet }
-      facet('scores') do 
-	range :score, [
-	  { from: 0, to: 19},
-	  { from: 20, to: 22}, 
-	  { from: 23, to: 24},
-	  { from: 25 }
-	]
-      end
+			facet('chains') {terms :company_id }
+			facet('auditors') {terms :auditor_facet }
+			facet('scores') do 
+      	range :score, [
+      		{ from: 0, to: 19},
+      		{ from: 20, to: 22}, 
+      		{ from: 23, to: 24},
+      		{ from: 25 }
+      	]
+			end
 
-      sort  {by :created_at, 'desc' } 
-    end
+			sort	{by :created_at, 'desc' } 
+		end
 
-    if tire_results.count > 0 
-      return_value[:results] = tire_results.results
-      facets = Hash.new
+		if tire_results.count > 0 
+			return_value[:results] = tire_results.results
+			facets = Hash.new
 
-      # Organizing Company Facet
-      if params[:company_id].present? || tire_results.facets['chains']['terms'].count > 0
-	facets['chains'] = []
-	tire_results.facets['chains']['terms'].each_with_index do |chain,index|
-	  chain[:company_name] = Company.find(chain['term'].to_i)[:name]
-	  facets['chains'].push(chain)
+			# Organizing Company Facet
+			if params[:company_id].present? || tire_results.facets['chains']['terms'].count > 0
+      	facets['chains'] = []
+      	tire_results.facets['chains']['terms'].each_with_index do |chain,index|
+      		chain[:company_name] = Company.find(chain['term'].to_i)[:name]
+      		facets['chains'].push(chain)
+      	end
+      	facets['chains'].sort!{ |a,b| a[:company_name].sub(/^(the|a|an)\s+/i, '') <=> b[:company_name].sub(/^(the|a|an)\s+/i, '')}
+			end
+
+			# Organizing Auditor Facet
+			if params[:auditor].present? || tire_results.facets['auditors']['terms'].count > 0
+      	facets['auditors'] = []
+      	tire_results.facets['auditors']['terms'].each_with_index do |auditor,index|
+      		facets['auditors'].push(auditor)
+      	end
+      	facets['auditors'].sort!{ |a,b| a['term'] <=> b['term']}
+			end
+
+			# Organizing Scores Facet
+			facets['scores'] = tire_results.facets['scores']['ranges']
+
+			return_value[:facets] = facets
+		end
+
+		return return_value
+
 	end
-	facets['chains'].sort!{ |a,b| a[:company_name].sub(/^(the|a|an)\s+/i, '') <=> b[:company_name].sub(/^(the|a|an)\s+/i, '')}
-      end
-
-      # Organizing Auditor Facet
-      if params[:auditor].present? || tire_results.facets['auditors']['terms'].count > 0
-	facets['auditors'] = []
-	tire_results.facets['auditors']['terms'].each_with_index do |auditor,index|
-	  facets['auditors'].push(auditor)
+	
+	def is_pending?
+		self[:status] == 0
 	end
-	facets['auditors'].sort!{ |a,b| a['term'] <=> b['term']}
-      end
-
-      # Organizing Scores Facet
-      facets['scores'] = tire_results.facets['scores']['ranges']
-
-      return_value[:facets] = facets
-    end
-
-    return return_value
-
-  end
-  
-  def is_pending?
-    self[:status] == 0
-  end
 end
