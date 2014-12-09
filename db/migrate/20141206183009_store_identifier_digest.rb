fa8d3d5c1ef241d478d64f1968610c0ce51d79ee
Abhishek Banerji <webdesign.banerji@gmail.com>
2014-12-09 12:43:35 -1000

    Migration updates

--- db/migrate/20141206183009_store_identifier_digest.rb
+++ db/migrate/20141206183009_store_identifier_digest.rb
@@ -1,5 +1,5 @@
 class StoreIdentifierDigest < ActiveRecord::Migration
-  def change
+  def up 
     add_column :stores, :active, :bool, :null => false, :default => true, :after => :longitude
     add_column :stores, :url_id, :string, :size => 256, :after => :id
     
@@ -12,5 +12,9 @@ class StoreIdentifierDigest < ActiveRecord::Migration
     add_index :stores, :url_id, :unique => true
     add_index :stores, [:active, :url_id]
     
+  end
+  def down
+    remove_column :stores, :active
+    remove_column :stores, :url_id
   end
 end
