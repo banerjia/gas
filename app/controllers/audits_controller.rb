3ff54b8a41638445ec83638c3be13c5c785007b1
Abhishek Banerji <webdesign.banerji@gmail.com>
2014-12-21 16:13:30 -1000

    Audits > Add New - in progress
    This may be the final database structure for the audits section. Some notes about this design:
    
    It has been concluded that the logic for computing the score will reside in the view using Angular. This logic will not be stored in the database. This will definitely cause problems in the future. In the event that audit sheet is changed and older audits need to be pulled, it may create problems in presenting the data that is historically accurate because the computational logic would have changed in the new view. So view versioning may have to be looked into.

--- app/controllers/audits_controller.rb
+++ app/controllers/audits_controller.rb
@@ -5,7 +5,7 @@ class AuditsController < ApplicationController
 	def new
 		store_id = params[:store_id] 
 		store = Store.find(store_id)
-		metrics_to_use = Metric.active_metrics.order([:display_order]).pluck(:id, :title, :description, :points_awarded, :include, :response_type)
+		metrics_to_use = Metric.active_metrics.order([:display_order])
 		new_audit = store.audits.build
     
     respond_to do |format|
